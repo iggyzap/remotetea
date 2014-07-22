be5b60879a4dfedb867b8072606c41c81351043b
hwirths <hwirths@nde.ag>
2014-07-22 16:08:21 +0200

    * The following flags are added to jrpcgen
      -t              create TCP Server only
      -u              create UDP Server only
      -noxdr          do not create XDR datatype classes
    
    * Typedefs such as 'typedef struct Structure Structure' in X - files are
    now recognized and ignored, just to get a little more compatible with
    the C - Variant of rpcgen.
    
    * Client-Stubs are now generated with a default constructor,
    instantiating a stub object without an RPC client obejct.
    
    * Server-Stubs are now generated with a default 'null procedure', if it
    is not mentioned in the coresponding X - file.

--- remotetea/src/org/acplt/oncrpc/apps/jrpcgen/jrpcgen.java
+++ remotetea/src/org/acplt/oncrpc/apps/jrpcgen/jrpcgen.java
@@ -84,7 +84,7 @@ public class jrpcgen {
     /**
      * Current version of jrpcgen.
      */
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
 
     /**
      * A remote procedure has no parameters and thus needs to use the
