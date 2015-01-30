6e3ad2a728fae116daf474bb8de334e8cb95e8b1
hwirths <hwirths@nde.ag>
2015-01-30 18:18:27 +0100

    Updated version to 1.1.2-SNAPSHOT after publishing release version 1.1.1

--- remotetea/jrpcgen/src/main/java/org/acplt/oncrpc/apps/jrpcgen/jrpcgen.java
+++ remotetea/jrpcgen/src/main/java/org/acplt/oncrpc/apps/jrpcgen/jrpcgen.java
@@ -84,7 +84,7 @@ public class jrpcgen {
     /**
      * Current version of jrpcgen.
      */
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2-SNAPSHOT";
 
     /**
      * A remote procedure has no parameters and thus needs to use the
